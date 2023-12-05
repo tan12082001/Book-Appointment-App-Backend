e23fe60533fb8750629461f14e1ee88d4729a86e
Wineshuga <uzochukwuwinnie@gmail.com>
2023-12-05 14:53:57 +0000

    Add route for reservation index

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     get 'all_cars', to: 'cars#index'
     get 'cars/:id', to: 'cars#show'
+    get 'my_reservations', to: 'my_reservations#index'
   end
 end
