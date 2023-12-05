2692c3095587e915f26c8eead5ceabf77f414c33
Wineshuga <uzochukwuwinnie@gmail.com>
2023-12-05 17:05:14 +0000

    Modify routes to create pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     get 'all_cars', to: 'cars#index'
     get 'cars/:id', to: 'cars#show'
+    post 'car/new_car', to: 'cars#create'
     get 'user/:user_id/my_reservations', to: 'my_reservations#index'
+    post 'user/:user_id/car/:car_id/new_reserve', to: 'my_reservations#create'
   end
 end
