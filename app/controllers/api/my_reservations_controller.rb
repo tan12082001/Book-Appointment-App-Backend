6411c8ff026535fd183175fb375e033181d8b411
Wineshuga <uzochukwuwinnie@gmail.com>
2023-12-05 17:07:24 +0000

    Fix rubocop issues

--- app/controllers/api/my_reservations_controller.rb
+++ app/controllers/api/my_reservations_controller.rb
@@ -1,19 +1,19 @@
 class Api::MyReservationsController < ApplicationController
   # before_action :authenticate_user!
-    
+
   def index
     @user = User.find(params[:user_id])
     @reservations = MyReservation.where(user_id: @user.id).includes(:car)
     reservations_json = @reservations.map do |item|
       {
-        "car" => {
-          "id" => item.car.id,
-          "name" => item.car.name
+        'car' => {
+          'id' => item.car.id,
+          'name' => item.car.name
         },
-        "date" => item.date,
-        "city" => item.city,
-        "created_at" => item.created_at,
-        "updated_at" => item.updated_at
+        'date' => item.date,
+        'city' => item.city,
+        'created_at' => item.created_at,
+        'updated_at' => item.updated_at
       }
     end
 
