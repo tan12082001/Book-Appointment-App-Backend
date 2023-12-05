5ce22279c1733d28e67d066f9c50fb3e7c51a6d3
Wineshuga <uzochukwuwinnie@gmail.com>
2023-12-05 15:42:07 +0000

    Modify my reservation index route to use user id

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     get 'all_cars', to: 'cars#index'
     get 'cars/:id', to: 'cars#show'
-    get 'my_reservations', to: 'my_reservations#index'
+    get 'user/:user_id/my_reservations', to: 'my_reservations#index'
   end
 end
