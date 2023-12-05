97ef622db17106cd497eae0d6c7c697d81c71fd9
Wineshuga <uzochukwuwinnie@gmail.com>
2023-12-05 19:16:29 +0000

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     get 'all_cars', to: 'cars#index'
     get 'cars/:id', to: 'cars#show'
     post 'car/new_car', to: 'cars#create'
-    get 'user/:user_id/my_reservations', to: 'my_reservations#index'
-    post 'user/:user_id/car/:car_id/new_reserve', to: 'my_reservations#create'
+    get 'my_reservations', to: 'my_reservations#index'
+    post 'car/:car_id/new_reserve', to: 'my_reservations#create'
   end
 end
