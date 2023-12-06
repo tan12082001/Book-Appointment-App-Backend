cfa3bcf6c54bfd6f3892e723983abf244de0e590
Wineshuga <uzochukwuwinnie@gmail.com>
2023-12-06 20:21:06 +0000

    Change localhost port to 4000

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,7 +56,9 @@ Rails.application.configure do
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 4000 }
 
+  config.navigational_formats = []
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
