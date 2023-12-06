8fdc26ae67f14080e49404e7a48e2087e33c6c56
Wineshuga <uzochukwuwinnie@gmail.com>
2023-12-06 20:28:03 +0000

    Modify User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :jwt_authenticatable, jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
+  devise :database_authenticatable, :registerable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
 
   has_many :cars, through: :my_reservations
   has_many :my_reservations
 
   validates :username, presence: true, uniqueness: true
+  validates :email, presence: true
 end
