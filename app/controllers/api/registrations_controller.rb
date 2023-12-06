3421186b46f365d9c0d7bbf832ff11f89c653ff7
Wineshuga <uzochukwuwinnie@gmail.com>
2023-12-06 20:32:29 +0000

    Modify registration controller   Handle error   Create params for user

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -2,9 +2,17 @@ class Api::RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
   def create
-    super do |resource|
-      render json: { message: 'Signup successful', user: resource }
-      return
+    user = User.new(user_params)
+    if user.save
+      render json: { message: 'Signup successful', user: user }
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
