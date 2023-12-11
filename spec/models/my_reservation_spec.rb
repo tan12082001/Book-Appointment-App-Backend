7517f9bf9e54db9692770faa10983a699e9337f3
suleiman gacheru <dretrevor8@gmail.com>
2023-12-11 17:50:48 +0300

    modify tests, modify order for associations in models

--- spec/models/my_reservation_spec.rb
+++ spec/models/my_reservation_spec.rb
@@ -1,13 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe MyReservation, type: :model do
-  describe 'validations' do
-    it { should validate_presence_of(:date) }
-    it { should validate_presence_of(:city) }
+  describe 'associations' do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:car) }
   end
 
-  describe 'associations' do
-    it { should belong_to(:user) }
-    it { should belong_to(:car) }
-  end
+  # Add tests for other validations here
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,44 +1,10 @@
+require 'rails_helper'
+
 RSpec.describe User, type: :model do
-  describe 'validations' do
-    it 'is valid with valid attributes' do
-      user = User.new(username: 'testuser', email: 'user@example.com', password: 'password')
-
-      expect(user).to be_valid
-    end
-
-    it 'is not valid without a username' do
-      user = User.new(email: 'user@example.com', password: 'password')
-
-      expect(user).not_to be_valid
-      expect(user.errors[:username]).to include("can't be blank")
-    end
-
-    it 'is not valid without an email' do
-      user = User.new(username: 'testuser', password: 'password')
-
-      expect(user).not_to be_valid
-      expect(user.errors[:email]).to include("can't be blank")
-    end
-
-    it 'is not valid with a duplicate username' do
-      User.create(username: 'testuser', email: 'user1@example.com', password: 'password')
-      user = User.new(username: 'testuser', email: 'user2@example.com', password: 'password')
-
-      expect(user).not_to be_valid
-      expect(user.errors[:username]).to include('has already been taken')
-    end
-  end
-
   describe 'associations' do
-    it 'has many cars through my_reservations' do
-      user = User.reflect_on_association(:cars)
-      expect(user.macro).to eq(:has_many)
-      expect(user.options[:through]).to eq(:my_reservations)
-    end
-
-    it 'has many my_reservations' do
-      user = User.reflect_on_association(:my_reservations)
-      expect(user.macro).to eq(:has_many)
-    end
+    it { is_expected.to have_many(:my_reservations) }
+    it { is_expected.to have_many(:cars).through(:my_reservations) }
   end
+
+  # Add tests for other validations here
 end
