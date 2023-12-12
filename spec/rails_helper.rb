0c09917d4953768c1637aa22e45eb00bfbef75c1
suleiman yusuf <108791588+hetrox8@users.noreply.github.com>
2023-12-12 11:45:50 +0300

    Update rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,7 @@ require 'shoulda/matchers'
 require 'database_cleaner'
 
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
